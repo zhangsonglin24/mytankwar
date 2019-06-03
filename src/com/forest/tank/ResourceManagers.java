4e0cc3da1bd64d09062b2e5ea17b2df5a771486a
zhangsonglin <17601220677@163.com>
2019-06-03 16:02:14 +0800

    美化图片

--- src/com/forest/tank/ResourceManagers.java
+++ src/com/forest/tank/ResourceManagers.java
@@ -13,14 +13,15 @@ public class ResourceManagers {
 
     static {
         try {
-            tankL = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
-            tankU = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
-            tankR = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
-            tankD = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
-            bulletL = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
-            bulletU = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
-            bulletR = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
-            bulletD = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            tankU = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
+            tankL = ImageUtil.rotateImage(tankU, -90);
+            tankR = ImageUtil.rotateImage(tankU, 90);
+            tankD = ImageUtil.rotateImage(tankU, 180);
+
+            bulletU = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/bulletU.png"));
+            bulletL = ImageUtil.rotateImage(bulletU, -90);
+            bulletR = ImageUtil.rotateImage(bulletU, 90);
+            bulletD = ImageUtil.rotateImage(bulletU, 180);
             for(int i=0; i<16; i++)
                 explodes[i] = ImageIO.read(ResourceManagers.class.getClassLoader().getResourceAsStream("images/e" + (i+1) + ".gif"));
         } catch (IOException e) {
